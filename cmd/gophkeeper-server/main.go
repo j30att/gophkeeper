49b2542fe39b4a9e9ed89ec971cb1cbed030b6e7
Игорь Ипполитов <Igor.Ippolitov@x5.ru>
2026-09-03 00:51:31 +0800

    feat: auth module

--- cmd/gophkeeper-server/main.go
+++ cmd/gophkeeper-server/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/igor/gophkeeper/internal/bootstrap"
@@ -14,6 +15,7 @@ var version = "0.0.1"
 // main запускает сервер GophKeeper.
 func main() {
 	if err := bootstrap.StartServer(context.Background(), version); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "server failed: %v\n", err)
 		os.Exit(1)
 	}
 }
